ec359af5786fb86d1ff9d13eec2778be7784c337
Gaetan Gogail <gaetan.gogail@ericsson.com>
2019-11-18 14:51:29 +0100

    update dictionnary NAS-152

--- src/main/java/com/ericsson/mts/nas/informationelement/field/wrapper/MessageWrapperField.java
+++ src/main/java/com/ericsson/mts/nas/informationelement/field/wrapper/MessageWrapperField.java
@@ -12,6 +12,7 @@ import com.ericsson.mts.nas.writer.FormatWriter;
 import java.io.IOException;
 
 import static com.ericsson.mts.nas.reader.XMLFormatReader.binaryToHex;
+import static com.ericsson.mts.nas.writer.XMLFormatWriter.bytesToHex;
 
 public class MessageWrapperField extends AbstractTranslatorField {
 
@@ -63,16 +64,5 @@ public class MessageWrapperField extends AbstractTranslatorField {
         }
 
         throw new DecodingException("Unknow key for value : "+ value);
-    }
-
-    private static String bytesToHex(byte[] bytes) {
-        char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars);
     }
 }
